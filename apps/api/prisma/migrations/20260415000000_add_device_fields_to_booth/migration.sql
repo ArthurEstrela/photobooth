a3fba1688507177dca2ba231a60755a01056a364
Arthur Estrela <arthurestrela369@gmail.com>
2026-04-15 13:55:11 -0300

    feat(api): add selectedCamera, selectedPrinter, maintenancePin to Booth

---
+++ apps/api/prisma/migrations/20260415000000_add_device_fields_to_booth/migration.sql
@@ -0,0 +1,8 @@
+-- AddColumn selectedCamera
+ALTER TABLE "Booth" ADD COLUMN "selectedCamera" TEXT;
+
+-- AddColumn selectedPrinter
+ALTER TABLE "Booth" ADD COLUMN "selectedPrinter" TEXT;
+
+-- AddColumn maintenancePin
+ALTER TABLE "Booth" ADD COLUMN "maintenancePin" TEXT;
